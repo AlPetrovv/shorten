6b1554777066e8ff430cd2413b3ee83bcbcfa8b6
aleksandr <a24923093@gmail.com>
2026-02-10 21:43:01 +0700

    feat: create queries for link

---
+++ app/src/db/queries.py
@@ -0,0 +1,4 @@
+CREATE_LINK = "INSERT INTO link (source_url, code) VALUES (?, ?)"
+SELECT_LINK_BY_CODE = "SELECT * FROM link WHERE code = ?"
+
+SELECT_LINK_BY_SOURCE = "SELECT * FROM link WHERE source_url = ?"
